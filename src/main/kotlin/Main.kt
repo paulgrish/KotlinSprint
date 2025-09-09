406f150f1b00d83ce4bf3c2a1c254e17914882b1
paulgri <paulgrishmanovsky@gmail.com>
2025-09-09 19:53:22 +0500

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "Kotlin"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Hello, " + name + "!")
+
+    val range = 1..5
+    for (i in range) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        println("i = $i")
+    }
+    //TIP Прикольный коммент, <a href="https://androidsprint.ru//files/_git-new-PART-2.mp4">Thanks!</a>
+    range.forEach { println("j = $it") }
+    println(range)
+    println(range.toList())
+}
