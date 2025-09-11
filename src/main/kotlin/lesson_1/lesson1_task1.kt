d543cf748348875375dd711d489d79e0cfa39897
paulgri <paulgrishmanovsky@gmail.com>
2025-09-11 20:47:45 +0500

    KS-1-1 task 1 fixed

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,6 +1,6 @@
 package lesson_1
 
-//TIP Задача 1 к Уроку 1.
+// Задача 1 к Уроку 1.
 //
 // Пишем логику экрана приложения для интернет-магазина. Представим, что у интернет-магазина было 75 заказов.
 // Нужно продумать релевантные названия и корректно объявить 2 переменные, которые будут содержать:
@@ -12,21 +12,7 @@ package lesson_1
 
 fun main() {
 
-    val maxNumberOfOrders = 75
-    var countOfCurrentOrders = 25
-    val thanksMessage = "Благодарим Вас за покупку в нашем интернет-магазине! Будем рады видеть Вас снова!"
-
-    // И чтобы не было варнингов из-за неиспользованных переменных
-    println("Your order #$countOfCurrentOrders in max $maxNumberOfOrders orders is complete")
-    countOfCurrentOrders--
-    println("Your order #$countOfCurrentOrders in max $maxNumberOfOrders orders is complete")
-    println(thanksMessage)
-    // но почему-то не вижу в консоли русский текст, только квадратики(
-
-    val widthOfDoorInCm = 80.0f
-    val heightOfDoorInCm = 200.0f
-    val materialForDoor = "wood"
-
-    println("Have you $materialForDoor door with size $widthOfDoorInCm x $heightOfDoorInCm cm?")
+    val countOfCurrentOrders: Int = 75
+    val thanksMessage: String = "Благодарим Вас за покупку в нашем интернет-магазине! Будем рады видеть Вас снова!"
 
 }
