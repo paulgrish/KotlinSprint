2039141c0334a219c7458552b6719e52fb4d119e
paulgri <paulgrishmanovsky@gmail.com>
2025-09-14 19:30:34 +0500

    KS-4-2 task 2 done но оччень много предупреждений из-за предопределенности результатов)

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -15,4 +15,18 @@ package lesson_4
  */
 fun main() {
 
+    var weight = 20
+    var volume = 80
+
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${weight > MIN_WEIGHT && weight <= MAX_WEIGHT && volume < MAX_VOLUME}")
+
+    weight = 50
+    volume = 100
+
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${weight > MIN_WEIGHT && weight <= MAX_WEIGHT && volume < MAX_VOLUME}")
+
 }
+
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_VOLUME = 100
