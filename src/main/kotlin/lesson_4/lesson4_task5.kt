a98e3efd23c94678645570aa29ca2544c18f245d
paulgri <paulgrishmanovsky@gmail.com>
2025-09-14 21:26:48 +0500

    KS-4-5 task 5 done

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -25,5 +25,38 @@ package lesson_4
  * только из логических операторов.
  */
 fun main() {
-    
+
+    var shipIsDamaged: Boolean
+    var crewSize: Int
+    var ratioAmount: Int
+    var weatherIsGood: Boolean
+
+    println("Введите исходные данные о судне:")
+    println("- наличие повреждений корпуса (true / false): ")
+    shipIsDamaged = readln().toBoolean()
+    println("- текущий состав экипажа (количество человек): ")
+    crewSize = readln().toInt()
+    println("- наличие провизии на борту (количество ящиков): ")
+    ratioAmount = readln().toInt()
+    println("- благоприятность метеоусловий (true / false): ")
+    weatherIsGood = readln().toBoolean()
+
+    val shipCanDeparture = shipIsDamaged == COND_DAMAGED && crewSize >= COND_MIN_CREW &&
+            crewSize <= COND_MAX_CREW && ratioAmount > COND_MIN_RATIO_MORE ||
+            shipIsDamaged == ALT_COND_DAMAGED && crewSize == ALT_COND_CREW &&
+            ratioAmount >= ALT_COND_MIN_RATIO && weatherIsGood == ALT_COND_GOOD_WEATHER
+
+    println("Этот корабль может отправиться в плавание: $shipCanDeparture")
+
 }
+
+const val COND_DAMAGED = false
+const val COND_MIN_CREW = 55
+const val COND_MAX_CREW = 70
+const val COND_MIN_RATIO_MORE = 50
+
+const val ALT_COND_DAMAGED = true
+const val ALT_COND_CREW = 70
+const val ALT_COND_MIN_RATIO = 50
+const val ALT_COND_GOOD_WEATHER = true
+
