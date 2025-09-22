57d253352f667e8255d801df279ee6b472c10bef
paulgri <paulgrishmanovsky@gmail.com>
2025-09-22 22:24:02 +0500

    KS-7-3 task 3 done

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -9,4 +9,20 @@ package lesson_7
  */
 fun main() {
 
+    var endNumber: Int
+
+    while (true) {
+        print("Введите положительное число: ")
+        endNumber = readln().toInt()
+        if (endNumber > 0)
+            break
+        println("Число должно быть положительным (больше 0)!")
+    }
+
+    println("Четные числа от 0 до $endNumber")
+    val numbersSet = 0 .. endNumber step 2
+    for (i in numbersSet) {
+        println(i)
+    }
+
 }
