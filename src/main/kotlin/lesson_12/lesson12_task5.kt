e8262af99c6a7dae774842ec34975c2d53d6de13
paulgri <paulgrishmanovsky@gmail.com>
2025-10-05 19:23:01 +0500

    KS-12-5 task 5 fix

--- src/main/kotlin/lesson_12/lesson12_task5.kt
+++ src/main/kotlin/lesson_12/lesson12_task5.kt
@@ -1,5 +1,7 @@
 package lesson_12
 
+import kotlin.random.Random
+
 /**
  * Задача 5* к Уроку 12
  *
@@ -21,23 +23,23 @@ fun main() {
 
     val monthWeatherData = List(DAYS_IN_MONTH) {
         DailyWeatherData(
-            dayKelvinTemperature = (DAY_TEMPERATURE_LOW .. DAY_TEMPERATURE_HI).random(),
-            nightKelvinTemperature = (NIGHT_TEMPERATURE_LOW .. NIGHT_TEMPERATURE_HI).random(),
-            _hasDayPrecipitation = (1 .. DAILY_PRECIPITATION_MAX).random() <= DAILY_PRECIPITATION_PERCENT
+            dayKelvinTemperature = Random.nextInt(DAY_TEMPERATURE_LOW, DAY_TEMPERATURE_HI + 1),
+            nightKelvinTemperature = Random.nextInt(NIGHT_TEMPERATURE_LOW, NIGHT_TEMPERATURE_HI + 1),
+            _hasDayPrecipitation = Random.nextInt(0, DAILY_PRECIPITATION_MAX) <= DAILY_PRECIPITATION_PERCENT
         )
     }
 
-    val dayTemperatureSet = monthWeatherData.map { it.dayCelsiusTemperature }
-    val nightTemperatureSet = monthWeatherData.map { it.nightCelsiusTemperature }
-    val dailyPrecipitationSet = monthWeatherData.map { it.hasDayPrecipitation }
+    val dayTemperatures = monthWeatherData.map { it.dayCelsiusTemperature }
+    val nightTemperatures = monthWeatherData.map { it.nightCelsiusTemperature }
+    val dailyPrecipitations = monthWeatherData.map { it.hasDayPrecipitation }
 
-    val dayTemperatureAverage = dayTemperatureSet.average()
-    val nightTemperatureAverage = nightTemperatureSet.average()
-    val percipitationDaysCount = dailyPrecipitationSet.filter { it }.size
+    val dayTemperatureAverage = dayTemperatures.average()
+    val nightTemperatureAverage = nightTemperatures.average()
+    val precipitationDaysCount = dailyPrecipitations.filter { it }.size
 
     println("Средняя дневная температура за $DAYS_IN_MONTH дней: %+.1f°C".format(dayTemperatureAverage))
     println("Средняя ночная температура за $DAYS_IN_MONTH дней: %+.1f°C".format(nightTemperatureAverage))
-    println("Количество дней с осадками за $DAYS_IN_MONTH дней: $percipitationDaysCount")
+    println("Количество дней с осадками за $DAYS_IN_MONTH дней: $precipitationDaysCount")
 
 }
 
@@ -51,8 +53,9 @@ class DailyWeatherData(
     val hasDayPrecipitation: Boolean = _hasDayPrecipitation
 
     fun printInfo() {
-        println("Температура днем %+d°C, ночью %+d°C, ".format(dayCelsiusTemperature, nightCelsiusTemperature) +
-                if (hasDayPrecipitation) "осадки" else "без осадков"
+        println(
+            "Температура днем %+d°C, ночью %+d°C, ".format(dayCelsiusTemperature, nightCelsiusTemperature) +
+                    if (hasDayPrecipitation) "осадки" else "без осадков"
         )
     }
 }
@@ -61,15 +64,10 @@ const val CELSIUS_ZERO_IN_KELVIN = 273
 
 const val DAYS_IN_MONTH = 30
 
-const val DAY_TEMPERATURE_BASE = 295
-const val DAY_TEMPERATURE_VARIATION = 5
-const val NIGHT_TEMPERATURE_DIFFERENCE = 9
-const val NIGHT_TEMPERATURE_BASE = DAY_TEMPERATURE_BASE - NIGHT_TEMPERATURE_DIFFERENCE
-const val NIGHT_TEMPERATURE_VARIATION = 7
 const val DAILY_PRECIPITATION_MAX = 100
-
-const val DAY_TEMPERATURE_HI = DAY_TEMPERATURE_BASE + DAY_TEMPERATURE_VARIATION
-const val DAY_TEMPERATURE_LOW = DAY_TEMPERATURE_BASE - DAY_TEMPERATURE_VARIATION
-const val NIGHT_TEMPERATURE_HI = NIGHT_TEMPERATURE_BASE + NIGHT_TEMPERATURE_VARIATION
-const val NIGHT_TEMPERATURE_LOW = NIGHT_TEMPERATURE_BASE - NIGHT_TEMPERATURE_VARIATION
 const val DAILY_PRECIPITATION_PERCENT = 68
+
+const val DAY_TEMPERATURE_HI = CELSIUS_ZERO_IN_KELVIN + 27
+const val DAY_TEMPERATURE_LOW = CELSIUS_ZERO_IN_KELVIN + 18
+const val NIGHT_TEMPERATURE_HI = CELSIUS_ZERO_IN_KELVIN + 20
+const val NIGHT_TEMPERATURE_LOW = CELSIUS_ZERO_IN_KELVIN + 15
