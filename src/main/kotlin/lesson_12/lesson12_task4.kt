df552b2014df42ecd198764dabf3bb0748372645
paulgri <paulgrishmanovsky@gmail.com>
2025-10-05 14:37:58 +0500

    KS-12-4 task 4 init

---
+++ src/main/kotlin/lesson_12/lesson12_task4.kt
@@ -0,0 +1,32 @@
+package lesson_12
+
+/**
+ * Задача 4 к Уроку 12
+ *
+ * Измени программу таким образом, чтобы информация о погоде выводилась в консоль сразу при создании объекта.
+ */
+fun main() {
+
+    val day1 = DailyWeather4(295, 289, true)
+
+    day1.printInfo()
+
+}
+
+class DailyWeather4(
+    dayKelvinTemperature: Int,
+    nightKelvinTemperature: Int,
+    _dailyPrecipitation: Boolean,
+) {
+    val dayCelsiusTemperature: Int = dayKelvinTemperature - CELSIUS_ZERO_IN_KELVIN
+    val nightCelsiusTemperature: Int = nightKelvinTemperature - CELSIUS_ZERO_IN_KELVIN
+    val dailyPrecipitation: Boolean = _dailyPrecipitation
+
+    fun printInfo() {
+        println("Температура днем %+d°C, ночью %+d°C, ".format(dayCelsiusTemperature, nightCelsiusTemperature) +
+                if (dailyPrecipitation) "осадки" else "без осадков"
+        )
+    }
+}
+
+const val CELSIUS_ZERO_IN_KELVIN = 273
