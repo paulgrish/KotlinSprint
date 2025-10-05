1e72f1fa2f62e8784e8bfe5370b104f684f7c264
paulgri <paulgrishmanovsky@gmail.com>
2025-10-05 13:28:12 +0500

    KS-12-1 task 1 done

--- src/main/kotlin/lesson_12/lesson12_task1.kt
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -18,4 +18,31 @@ package lesson_12
  */
 fun main() {
 
+    val day1 = DailyWeather()
+
+    day1.dayCelsiusTemperature = 22f
+    day1.nightCelsiusTemperature = 17f
+
+    val day2 = DailyWeather()
+
+    day2.dayCelsiusTemperature = -1.5f
+    day2.nightCelsiusTemperature = -5.2f
+    day2.dailyPrecipitation = true
+
+    day1.printInfo()
+    day2.printInfo()
+
+}
+
+class DailyWeather {
+
+    var dayCelsiusTemperature: Float = +4.0f
+    var nightCelsiusTemperature: Float = -1.0f
+    var dailyPrecipitation: Boolean = false
+
+    fun printInfo() {
+        println("Температура днем %+.1f°C, ночью %+.1f°C, ".format(dayCelsiusTemperature, nightCelsiusTemperature) +
+            if (dailyPrecipitation) "осадки" else "без осадков"
+        )
+    }
 }
