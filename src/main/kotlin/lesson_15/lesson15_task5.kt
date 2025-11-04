93c58d6c37656cb24142f0f677834e205df6fb5e
paulgri <paulgrishmanovsky@gmail.com>
2025-11-04 20:56:40 +0500

    KS-15-5 task 5 done

--- src/main/kotlin/lesson_15/lesson15_task5.kt
+++ src/main/kotlin/lesson_15/lesson15_task5.kt
@@ -15,4 +15,135 @@ package lesson_15
  */
 fun main() {
 
+    Car().apply {
+        loadPassengers(8)
+        loadPassengers(0)
+        loadCargo(1.0f)
+        transfer()
+        unload()
+    }
+
+    Car().apply {
+        loadPassengers(2)
+        loadPassengers(1)
+        transfer()
+        unload()
+    }.apply {
+        loadPassengers(1)
+        transfer()
+        unload()
+    }
+
+    Truck().apply {
+        loadPassengers(2)
+        loadCargo(2.8f)
+        transfer()
+        unload()
+    }
+
+    Truck().apply {
+        loadPassengers(1)
+        loadCargo(0.8f)
+        transfer()
+        unload()
+    }.apply {
+        loadCargo(0.95f)
+        loadCargo(0.25f)
+        transfer()
+        unload()
+    }.apply {
+        loadPassengers(1)
+        transfer()
+        unload()
+    }
 }
+
+interface Dockable {
+    fun loadPassengers(count: Int)
+    fun loadCargo(amount: Float)
+    fun unload()
+}
+
+interface Movable {
+    fun onLine() {
+        println("*** Транспорт ${this::class.simpleName} вышел на линию.")
+    }
+    fun transfer()
+}
+
+abstract class Transport : Dockable, Movable {
+    init {
+        onLine()
+    }
+}
+
+class Car : Transport() {
+    var passengers = 0
+
+    override fun loadPassengers(count: Int) {
+        if (count > 0 && count <= passengers + CAR_MAX_PASSENGERS) {
+            passengers += count
+            println("Принял $count пассажиров")
+        } else {
+            println("Не могу принять $count пассажиров")
+        }
+    }
+
+    override fun loadCargo(amount: Float) {
+        println("Не могу принимать груз")
+    }
+
+    override fun unload() {
+        println("Доставил $passengers пассажиров")
+        passengers = 0
+    }
+
+    override fun transfer() {
+        if (passengers > 0) {
+            println("Перевожу $passengers пассажиров")
+        } else {
+            println("Некого перевозить")
+        }
+    }
+}
+
+class Truck : Transport() {
+    var passengers = 0
+    var cargo = 0.0f
+
+    override fun loadPassengers(count: Int) {
+        if (count > 0 && count <= passengers + TRUCK_MAX_PASSENGERS) {
+            passengers += count
+            println("Принял $count пассажиров")
+        } else {
+            println("Не могу принять $count пассажиров")
+        }
+    }
+
+    override fun loadCargo(amount: Float) {
+        if (amount > 0 && amount <= cargo + TRUCK_MAX_CARGO) {
+            cargo += amount
+            println("Принял $amount тонн груза")
+        } else {
+            println("Не могу принять $amount тонн груза")
+        }
+    }
+
+    override fun unload() {
+        println("Доставил $passengers пассажиров и $cargo тонн груза")
+        passengers = 0
+        cargo = 0.0f
+    }
+
+    override fun transfer() {
+        if (passengers > 0 || cargo > 0.0f) {
+            println("Перевожу $passengers пассажиров и $cargo тонн груза")
+        } else {
+            println("Нечего перевозить.")
+        }
+    }
+}
+
+const val CAR_MAX_PASSENGERS = 3
+const val TRUCK_MAX_PASSENGERS = 1
+const val TRUCK_MAX_CARGO = 2.0
