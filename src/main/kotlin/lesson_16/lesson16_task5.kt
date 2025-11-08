6fccdcbf1117cb4c5ebef52917db7282c80e6618
paulgri <paulgrishmanovsky@gmail.com>
2025-11-08 22:23:08 +0500

    KS-16-5 done

--- src/main/kotlin/lesson_16/lesson16_task5.kt
+++ src/main/kotlin/lesson_16/lesson16_task5.kt
@@ -1,5 +1,7 @@
 package lesson_16
 
+import kotlin.math.*
+
 /**
  * Задача 5* к Уроку 16
  *
@@ -12,5 +14,57 @@ package lesson_16
  * Сымитируй бой, вызвав несколько раз методы урона и лечения. В конце игрок должен умереть.
  */
 fun main() {
-
+    val gamer = Gamer("Bob")
+    gamer.printInfo()
+    gamer.damage(30)
+    gamer.printInfo()
+    gamer.cure(50)
+    gamer.printInfo()
+    gamer.damage(27)
+    gamer.printInfo()
+    gamer.damage(54)
+    gamer.printInfo()
+    gamer.cure(50)
+    gamer.printInfo()
+    gamer.damage(50)
+    gamer.printInfo()
+    gamer.damage(50)
+    gamer.printInfo()
+    gamer.cure(99)
+    gamer.printInfo()
+    gamer.damage(50)
+    gamer.printInfo()
 }
+
+class Gamer(
+    val name: String,
+) {
+    private var health = MAX_HEALTH
+    private var force = MAX_FORCE
+    private var alive = true
+
+    private fun toDie(){
+        health = 0
+        force = 0
+        alive = false
+    }
+
+    fun damage(points: Int) {
+        health = max(health - points, 0)
+        if (alive && health == 0)
+            toDie()
+    }
+
+    fun cure(points: Int) {
+        if (alive) {
+            health = min(health + points, MAX_HEALTH)
+        }
+    }
+
+    fun printInfo() {
+        println("Игрок \"$name\":\n\tЗдоровье: $health\n\tСила: $force\n\tСтатус: " + if (alive) "жив" else "мертв")
+    }
+}
+
+const val MAX_HEALTH = 100
+const val MAX_FORCE = 100
