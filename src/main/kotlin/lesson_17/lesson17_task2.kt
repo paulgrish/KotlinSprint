4e5b287217a1a37501938d45616a4aa6406bbcaa
paulgri <paulgrishmanovsky@gmail.com>
2025-11-09 12:51:50 +0500

    KS-17-2 init

---
+++ src/main/kotlin/lesson_17/lesson17_task2.kt
@@ -0,0 +1,37 @@
+package lesson_17
+
+/**
+ * Задача 2 к Уроку 17
+ *
+ * Опиши класс для корабля в мобильной игре. Поля хранят имя, среднюю скорость и порт приписки.
+ *
+ * Имя корабля менять нельзя, при попытке перезаписать имя в консоль должно выводиться предупреждение.
+ *
+ * Создай экземпляр класса и протестируй работу.
+ */
+fun main() {
+    val ship = Ship("Смелый", 30, "Мурманск")
+    ship.printInfo()
+    ship.homeport = "Владивосток"
+    ship.printInfo()
+    ship.speed = 35
+    ship.printInfo()
+    ship.name = "Отважный"
+    ship.printInfo()
+}
+
+class Ship(
+    name_: String,
+    var speed: Int,
+    var homeport: String,
+) {
+    var name: String = name_
+        get() = field
+        set(value) {
+            println("Имя корабля менять нельзя!")
+        }
+
+    fun printInfo() {
+        println("Корабль \"$name\": скорость $speed узлов, порт приписки $homeport")
+    }
+}
