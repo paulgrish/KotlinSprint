b341d168a8453fe2187247ea094411b67f9d3ddd
paulgri <paulgrishmanovsky@gmail.com>
2025-12-06 21:45:37 +0500

    KS-18-5 done

--- src/main/kotlin/lesson_18/lesson18_task5.kt
+++ src/main/kotlin/lesson_18/lesson18_task5.kt
@@ -11,5 +11,37 @@ package lesson_18
  * Используйте перегрузку методов для реализации.
  */
 fun main() {
+    val s = Screen()
+    s.draw(7, 8)
+    s.draw(1, 2, 3)
+    s.draw(5, 2, 4, 6)
+    s.draw(-1.2f, -3.4f)
+    s.draw(7.6f, 5.4f, 3.2f)
+    s.draw(2.3f, -4.5f, 6.7f, 8.9f)
+}
 
+class Screen {
+    fun draw(x: Int, y: Int) {
+        println("Point at ($x; $y)")
+    }
+
+    fun draw(x: Int, y: Int, r: Int) {
+        println("Circle at ($x; $y), radius $r")
+    }
+
+    fun draw(x: Int, y: Int, w: Int, h: Int) {
+        println("Circle at ($x; $y), width $w, height $h")
+    }
+
+    fun draw(x: Float, y: Float) {
+        println("Point at ($x; $y)")
+    }
+
+    fun draw(x: Float, y: Float, r: Float) {
+        println("Circle at ($x; $y), radius $r")
+    }
+
+    fun draw(x: Float, y: Float, w: Float, h: Float) {
+        println("Circle at ($x; $y), width $w, height $h")
+    }
 }
