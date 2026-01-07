4bad9f76f8952514a89fd4a97948350ee2f7eaf8
paulgri <paulgrishmanovsky@gmail.com>
2026-01-07 18:15:53 +0700

    KS-21-1 done

--- src/main/kotlin/lesson_21/lesson21_task1.kt
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -7,5 +7,26 @@ package lesson_21
  * Назови функцию vowelCount. Например, для строки "hello" функция должна вернуть 2.
  */
 fun main() {
+    val strings = listOf(
+        "Hello",
+        "Привет!",
+        "Назови функцию vowelCount.",
+    )
 
+    for (str in strings) {
+        println("В строке \"$str\" гласных ${str.vowelCount()}")
+    }
+}
+
+fun String.vowelCount(): Int {
+    val vowels = listOf(
+        'a', 'e', 'i', 'o', 'u', 'y',
+        'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я',
+    )
+    var count = 0
+    for (c in this) {
+        if (c.lowercaseChar() in vowels)
+            count++
+    }
+    return count
 }
