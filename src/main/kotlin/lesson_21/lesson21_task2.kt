b57ff1e67efeb60a7d0ea0d49892f607089cc616
paulgri <paulgrishmanovsky@gmail.com>
2026-01-11 00:07:32 +0500

    KS-21-2 done

--- src/main/kotlin/lesson_21/lesson21_task2.kt
+++ src/main/kotlin/lesson_21/lesson21_task2.kt
@@ -7,5 +7,17 @@ package lesson_21
  * чисел в списке. Четными считаются числа, которые делятся на 2 без остатка.
  */
 fun main() {
+    listOf(
+        listOf(1, 2, 3, 4, 5),
+        listOf(1, 3, 5),
+        listOf(2),
+    ).forEach {
+        println("Для списка $it сумма четных равна ${it.evenNumbersSum()}")
+    }
+}
 
+fun List<Int>.evenNumbersSum(): Int {
+    return this.filter {
+        it % 2 == 0
+    }.sum()
 }
