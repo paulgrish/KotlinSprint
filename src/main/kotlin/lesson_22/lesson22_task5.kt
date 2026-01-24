c27ae3d4177a4ab1f33b68bea9ba40b70ff5ecb3
paulgrish <paulgrishmanovsky@gmail.com>
2026-01-24 22:22:36 +0300

    KS-22-5 not well (deprecated)

--- src/main/kotlin/lesson_22/lesson22_task5.kt
+++ src/main/kotlin/lesson_22/lesson22_task5.kt
@@ -1,5 +1,7 @@
 package lesson_22
 
+import java.util.Date
+
 /**
  * Задача 5* к Уроку 22
  *
@@ -15,5 +17,22 @@ package lesson_22
  * функции data class.
  */
 fun main() {
-
+    val record1 = GalacticGuide(
+        "Alpha Centauri",
+        "Тройная звездная система, ближайшая до Земли",
+        Date(2026, 1, 24, 22, 14, 18),
+        4.36f
+    )
+    println("Запись 1: Информация о месте:")
+    println(record1.component1())
+    println(record1.component2())
+    println(record1.component3())
+    println(record1.component4())
 }
+
+data class GalacticGuide(
+    val name: String,
+    val description: String,
+    val dateTime: Date,
+    val distance: Float,
+)
